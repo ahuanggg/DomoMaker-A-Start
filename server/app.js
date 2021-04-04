8e54efacaa2df0dda5112bf5fe163fa7f2983972
ahuanggg <ah6595@rit.edu>
2021-04-03 23:38:50 -0400

    domo maker A

---
+++ server/app.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const dbURL = 'mongodb+srv://ahuanggg:Andyxie130@cluster0.styc6.mongodb.net/DomoMaker?authSource=admin&replicaSet=atlas-143hk9-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+
+const mongooseOptions = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+};
+
+mongoose.connect(dbURL, mongooseOptions, (err) => {
+	if (err) {
+		console.log('Could not connect to database');
+		throw err;
+	}
+});
+
+const router = require('./router.js');
+
+const app = express();
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.engine(
+	'handlebars',
+	expressHandlebars({
+		defaultLayout: '',
+	})
+);
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(cookieParser());
+
+router(app);
+
+app.listen(port, (err) => {
+	if (err) {
+		throw err;
+	}
+	console.log(`Listening on port ${port}`);
+});
